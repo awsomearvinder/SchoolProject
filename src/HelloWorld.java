e853891df15ebb087f80126fc75c3ca54a144cd8
Arvinder Dhanoa <awsomearvinder@gmail.com>
2021-03-15 11:22:19 -0500

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("I've always wanted to learn pijul. But I just use git. :(");
+		System.out.println("Oh well, so do most people though.");
+	}
+}
